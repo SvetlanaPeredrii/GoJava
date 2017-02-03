a725d14a60263e4d83c28c7faac2be534a4d2c41
kaganets.s <sv_peredriy@mail.ru>
2017-02-03 12:20:07 +0200

    My second commit

---
+++ src/Module1/Fifth.java
@@ -0,0 +1,10 @@
+package Module1;
+
+/**
+ * Created by kaganets.s on 03.02.2017.
+ */
+public class Fifth {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
