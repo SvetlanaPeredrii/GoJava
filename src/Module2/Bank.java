85362a870784661c5c1839597f3e949fb8fe97a8
Svetlana Peredrii <sv_peredriy@mail.ru>
2017-02-10 17:14:48 +0200

    3 commit

---
+++ src/Module2/Bank.java
@@ -0,0 +1,21 @@
+package Module2;
+import java.util.Scanner;
+public class Bank {
+    public static void main(String[] args) {
+     withdraws();
+    }
+    private static void withdraws () {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input balance: ");
+        double balance = in.nextDouble();
+        System.out.print("Input withdrawal: ");
+        double withdrawal = in.nextDouble();
+        if (withdrawal*1.05 <= balance){
+            balance -= withdrawal * 1.05;
+            withdrawal = withdrawal * 0.05;
+        System.out.println("OK " + "   Withdrawal: " + withdrawal + "   Balance: " + balance);}
+        else System.out.println("NO ");
+
+    }
+
+}
