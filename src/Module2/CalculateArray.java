89facc95a25c2627cb6bf0b9cd8301ac3ca60b12
Svetlana Peredrii <sv_peredriy@mail.ru>
2017-02-10 16:26:54 +0200

    Second commit

--- src/Module2/CalculateArray.java
+++ src/Module2/CalculateArray.java
@@ -4,8 +4,8 @@ public class CalculateArray {
 
     public static void main(String[] args) {
 
-        int[] array = {-5, 90, 750, -234, 45, 99, 1, 34, -5, 46};
-        double[] array2 = {5.5, -9.7, -75, 234.89, -45.987, 9985.01, 10, -34.234, 5.98, 46.007};
+        int[] array = {-5, 90, 0, -234, 45, 99, 1, 34, -5, 46};
+        double[] array2 = {5.5, -9.7, -75, 234.89, -45.987, 9985.01, 2, -34.234, -5.98, 46.007};
         int[] array3 = {-500, -9000, -7500, -234, -4534, -9985, -100, -3443, -5000, -4600};
         double[] array4 = {-500.5, -9000.7, -7500, -234.89, -4534.987, -9985.01, -100, -3443.234, -5000.98, 460.007};
         sum(array);
@@ -18,6 +18,10 @@ public class CalculateArray {
         maxPositive(array4);
         multiplication(array);
         multiplication(array2);
+        modulus(array);
+        modulus(array2);
+        secondLargest(array);
+        secondLargest(array2);
     }
 
 
@@ -134,6 +138,7 @@ public class CalculateArray {
 
         for (int i = 0; i < array.length; i++) {
             multiplication *= array[i];
+            if (array[i]==0) {break;}
         }
         System.out.println("multiplication array = " + multiplication);
     }
@@ -143,7 +148,54 @@ public class CalculateArray {
 
         for (int i = 0; i < array2.length; i++) {
             multiplication *= array2[i];
+            if (array2[i]==0) {break;}
         }
         System.out.println("multiplication array2 = " + multiplication);
     }
+    private static void modulus(int[] array) {
+
+        int modulus = array[0] % array[array.length - 1];
+
+        System.out.println("modulus array = " + modulus);
+    }
+
+    private static void modulus(double[] array2) {
+
+        double modulus = array2[0] % array2[array2.length - 1];
+
+        System.out.println("modulus array2 = " + modulus);
+    }
+    private static void secondLargest(int[] array) {
+        int max = array[0];
+        int secondlargest = array[1];
+
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                secondlargest = max;
+                max = array[i];
+            }
+            else if (secondlargest < array[i])  {
+                secondlargest = array[i];
+            }
+         }
+        System.out.println("secondLargest " + "array = " + secondlargest);
+
+    }
+    private static void secondLargest(double[] array2) {
+
+        double max = array2[0];
+        double secondlargest = array2[1];
+
+        for (int i = 1; i < array2.length; i++) {
+            if (max < array2[i]) {
+                secondlargest = max;
+                max = array2[i];
+            }
+            else if (secondlargest < array2[i])  {
+                secondlargest = array2[i];
+            }
+        }
+        System.out.println("secondLargest " + "array2 = " + secondlargest);
+    }
+
 }
