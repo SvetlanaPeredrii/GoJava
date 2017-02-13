0640d842dd08156ad6eabd2ce628644c26361556
Svetlana Peredrii <sv_peredriy@mail.ru>
2017-02-13 20:50:53 +0200

    4 commit

---
+++ src/Module3/Bird.java
@@ -0,0 +1,17 @@
+package Module3;
+
+
+public class Bird {
+
+    static void sing(){
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am Bird");
+
+    }
+
+    public static void main(String[] args) {
+        sing();
+    }
+}
