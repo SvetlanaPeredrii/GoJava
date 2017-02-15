9887b68ddb7721e5dbfa6be3af3dda5e9e7b4738
kaganets.s <sv_peredriy@mail.ru>
2017-02-15 12:36:51 +0200

    6 commit

--- src/Module3/Bird.java
+++ src/Module3/Bird.java
@@ -3,15 +3,19 @@ package Module3;
 
 public class Bird {
 
-    static void sing(){
-        System.out.println("I am walking");
-        System.out.println("I am flying");
-        System.out.println("I am singing");
-        System.out.println("I am Bird");
+    static void sing(String vote1, String vote2, String vote3, String vote4){
+        System.out.println(vote1);
+        System.out.println(vote2);
+        System.out.println(vote3);
+        System.out.println(vote4);
 
     }
 
     public static void main(String[] args) {
-        sing();
+        String vote1 = "I am walking";
+        String vote2 = "I am flying";
+        String vote3 = "I am singing";
+        String vote4 = "I am Bird";
+        sing(vote1,vote2,vote3,vote4);
     }
 }
