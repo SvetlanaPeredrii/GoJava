7a11487e7410d205691e939184160249f82526d9
kaganets.s <sv_peredriy@mail.ru>
2017-03-09 17:07:44 +0200

    20 commit

--- src/module5/Main.java
+++ src/module5/Main.java
@@ -1,11 +1,20 @@
 package module5;
 
+
+import java.util.Date;
+
 /**
  * Created by kaganets.s on 09.03.2017.
  */
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Room room = new Room(111, 100, 5, new Date(02-03-2017), "Hayat2", "Ukraine" );
+        Room room2 = new Room(222, 100, 5, new Date(02-02-2016), "Hayat", "Ukraine" );
+
+        System.out.println("room equals room2 "+ room.equals(room2));
+        System.out.println("room hashCode room2 "+ (room.hashCode() == room2.hashCode()));
+
     }
 }
