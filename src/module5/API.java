ef4f79100c20f6c0d8c88d2070e5d59770dddb27
kaganets.s <sv_peredriy@mail.ru>
2017-03-09 18:30:39 +0200

    20 commit

--- src/module5/API.java
+++ src/module5/API.java
@@ -1,21 +1,48 @@
 package module5;
 
+import java.util.Date;
+
 /**
  * Created by kaganets.s on 09.03.2017.
  */
 public interface API {
 
 
-    class APIImpl1 implements API{
+    Room[] findRooms(int price, int persons, String city, String hotel);
 
+
+    class BookingComAPI implements API{
+        Room[] rooms;
+
+        public BookingComAPI() {
+
+            Room room = new Room(111, 100, 5, new Date(02-03-2017), "Hayat2", "Ukraine" );
+            Room room2 = new Room(222, 100, 5, new Date(02-02-2016), "Hayat", "Ukraine" );
+
+          rooms = new Room[]{room, room2};
+
+        }
+
+        @Override
+        public Room[] findRooms(int price, int persons, String city, String hotel) {
+            return new Room[0];
+        }
     }
 
-    class APIImpl2 implements API{
+    class TripAdvisorAPI implements API{
 
+        @Override
+        public Room[] findRooms(int price, int persons, String city, String hotel) {
+            return new Room[0];
+        }
     }
 
-    class APIImpl3 implements API{
+    class GoogleAPI implements API{
 
+        @Override
+        public Room[] findRooms(int price, int persons, String city, String hotel) {
+            return new Room[0];
+        }
     }
 
 }
--- src/module5/Main.java
+++ src/module5/Main.java
@@ -12,6 +12,10 @@ public class Main {
 
         Room room = new Room(111, 100, 5, new Date(02-03-2017), "Hayat2", "Ukraine" );
         Room room2 = new Room(222, 100, 5, new Date(02-02-2016), "Hayat", "Ukraine" );
+Room room3 = new Room(222, 100, 5, new Date(02-02-2016), "Hayat", "Ukraine");
+        API.BookingComAPI a = new API.BookingComAPI();
+      
+
 
         System.out.println("room equals room2 "+ room.equals(room2));
         System.out.println("room hashCode room2 "+ (room.hashCode() == room2.hashCode()));
