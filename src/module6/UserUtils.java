fdb60e621a758c3c90c9e39cc4a136c211b7a9ad
kaganets.s <sv_peredriy@mail.ru>
2017-03-31 18:22:17 +0300

    28 commit module6

---
+++ src/module6/UserUtils.java
@@ -0,0 +1,59 @@
+package module6;
+
+/**
+ * Created by kaganets.s on 31.03.2017.
+ */
+public class UserUtils {
+
+//    User[] uniqueUsers(User[] users){
+//        for (User user:users
+//             ) {
+//            if ()
+//
+//        }
+                
+
+   // }
+
+    User[] usersWithContitionalBalance(User[] users, int balance){
+
+        User[] usersWithContitionalBalance = new User[users.length];
+        int i = 0;
+
+        for (User user:users
+             ) {if(balance==user.getBalance()){
+
+            usersWithContitionalBalance[i] = user;
+            i++;
+        }
+
+        }
+
+        return usersWithContitionalBalance;
+    }
+
+    User[]  paySalaryToUsers(User[] users){
+
+        User[] paySalaryToUsers = new User[users.length];
+        int i =0;
+
+        for (User user:users
+             ) {
+
+            int balance=paySalaryToUsers[i].getBalance()+paySalaryToUsers[i].getSalary();
+            paySalaryToUsers[i].getBalance()=balance;
+
+            i++;
+
+        }
+
+        return paySalaryToUsers;
+    }
+//    long[] getUsersId(User[] users){
+//        long[] getUsersId;
+//        return getUsersId;
+//    }
+//    User[] deleteEmptyUsers(User[] users){
+//return
+//    }
+}
---
+++ src/module6/UsersMain.java
@@ -0,0 +1,25 @@
+package module6;
+
+import java.util.Arrays;
+
+/**
+ * Created by kaganets.s on 31.03.2017.
+ */
+public class UsersMain {
+    public static void main(String[] args) {
+
+        User user = new User(111, "Svetlana", "Kahanets", 5000, 1500);
+        User user2 = new User(222, "Ira", "Bodrova", 12345, 0);
+        User user3 = new User(333, "Dima", "Petrov", 500, 5500);
+        User user4 = new User(444, "Yulya", "Vasina", 236, 563);
+        User user5 = new User(111, "Svetlana", "Kahanets", 5000, 1500);
+        User[] users = {user,user2,user3,user4,user5};
+
+        UserUtils a = new UserUtils();
+        System.out.println(",,= "+ Arrays.toString(a.usersWithContitionalBalance(users,1500)));
+        System.out.println("salary= "+ Arrays.toString(a.paySalaryToUsers(users)));
+
+
+
+    }
+}
