c9f77a5168f2278dc76fa205dd4bdac2a645bd3b
kaganets.s <sv_peredriy@mail.ru>
2017-04-03 18:29:15 +0300

    30 commit module6

--- src/module6/UserUtils.java
+++ src/module6/UserUtils.java
@@ -5,15 +5,38 @@ package module6;
  */
 public class UserUtils {
 
-//    User[] uniqueUsers(User[] users){
+    User[] uniqueUsers(User[] users){
+
+    User [] uniqueUsers = new User[users.length];
+        int i = 0;
+
 //        for (User user:users
 //             ) {
-//            if ()
-//
+//            if (user != null) {
+//                uniqueUsers[i] = user;
+//                i++;
+//            }
 //        }
-                
+        for (User user1 : users
+                        ) {
+            if (user1 != null) {
+                for (User user2 : users
+                     ) {
+                    if (user2 != null) {
 
-   // }
+                        if (user2.equals(user1)) {
+
+                            uniqueUsers[i] = user1;
+                            i++;
+
+                        }
+                    }
+            }
+        }
+        }
+
+        return uniqueUsers;
+    }
 
     User[] usersWithContitionalBalance(User[] users, int balance){
 
@@ -21,39 +44,82 @@ public class UserUtils {
         int i = 0;
 
         for (User user:users
-             ) {if(balance==user.getBalance()){
 
-            usersWithContitionalBalance[i] = user;
-            i++;
+             ) {
+            if (user != null) {
+                if (balance == user.getBalance()) {
+
+                    usersWithContitionalBalance[i] = user;
+                    i++;
+                }
+            }else i++;
         }
-
-        }
-
         return usersWithContitionalBalance;
     }
 
-//    User[]  paySalaryToUsers(User[] users){
-//
-//        User[] paySalaryToUsers = new User[users.length];
-//        int i =0;
-//
-//        for (User user:users
-//             ) {
-//
-//            int balance=paySalaryToUsers[i].getBalance()+paySalaryToUsers[i].getSalary();
-//            paySalaryToUsers[i].getBalance()=balance;
-//
-//            i++;
-//
-//        }
-//
-//        return paySalaryToUsers;
-//    }
-//    long[] getUsersId(User[] users){
-//        long[] getUsersId;
-//        return getUsersId;
-//    }
-//    User[] deleteEmptyUsers(User[] users){
-//return
-//    }
+    User[]  paySalaryToUsers(User[] users){
+
+        User[] paySalaryToUsers = new User[users.length];
+
+        int i =0;
+
+        for (User user:users
+             ) {
+            if(users[i]!=null) {
+                paySalaryToUsers[i] = new User(users[i].getId(), users[i].getFirstName(), users[i].getLastName(), users[i].getSalary(), users[i].getBalance() + users[i].getSalary());
+
+                i++;
+            }
+            else i++;
+        }
+
+        return paySalaryToUsers;
+    }
+    long[] getUsersId(User[] users){
+        long[] getUsersId = new long[users.length];
+        int i = 0;
+        for (User user:users
+                ) {
+            if(users[i]!=null) {
+
+                getUsersId[i] = user.getId();
+
+                i++;
+           }
+        else i++;
+        }
+
+        return getUsersId;
+    }
+
+
+    User[] deleteEmptyUsers(User[] users){
+
+        int nullCount = 0;
+
+        for (User user:users
+                ) {
+            if(user==null) {
+
+                nullCount++;
+
+            }
+        }
+        int newLength = users.length - nullCount;
+
+        User[] deleteEmptyUsers = new User[newLength];
+
+        int iterator = 0;
+
+
+        for (User user:users
+                ) {
+            if (user != null) {
+                deleteEmptyUsers[iterator] = user;
+                iterator++;
+            }
+        }
+
+return deleteEmptyUsers;
+    }
 }
--- src/module6/UsersMain.java
+++ src/module6/UsersMain.java
@@ -13,11 +13,15 @@ public class UsersMain {
         User user3 = new User(333, "Dima", "Petrov", 500, 5500);
         User user4 = new User(444, "Yulya", "Vasina", 236, 563);
         User user5 = new User(111, "Svetlana", "Kahanets", 5000, 1500);
-        User[] users = {user,user2,user3,user4,user5};
+        User[] users = {user,user2,null,user3,user4,user5,null};
 
         UserUtils a = new UserUtils();
-        System.out.println(",,= "+ Arrays.toString(a.usersWithContitionalBalance(users,1500)));
-       // System.out.println("salary= "+ Arrays.toString(a.paySalaryToUsers(users)));
+        System.out.println("uniqueUsers= "+ Arrays.toString(a.uniqueUsers(users)));
+        System.out.println("usersWithContitionalBalance= "+ Arrays.toString(a.usersWithContitionalBalance(users,1500)));
+        System.out.println("salary= "+ Arrays.toString(a.paySalaryToUsers(users)));
+        System.out.println("UsersId= "+ Arrays.toString(a.getUsersId(users)));
+        System.out.println("deleteEmptyUsers= "+ Arrays.toString(a.deleteEmptyUsers(users)));
+
 
 
 
