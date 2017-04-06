6f0ac07a349d522b422fd1d110841d9cf96a2213
kaganets.s <sv_peredriy@mail.ru>
2017-04-06 11:28:01 +0300

    33 commit module7

---
+++ src/module7/Order.java
@@ -0,0 +1,16 @@
+package module7;
+
+import Module4.Task4_1.Currency;
+
+/**
+ * Created by kaganets.s on 06.04.2017.
+ */
+public class Order {
+
+   private long id;
+    private int price;
+    private Currency currency;
+    private String itemName;
+    private String shopIdentificator;
+    private User user;
+}
---
+++ src/module7/User.java
@@ -0,0 +1,13 @@
+package module7;
+
+/**
+ * Created by kaganets.s on 06.04.2017.
+ */
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private int balance;
+}
