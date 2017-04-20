8987effaeec90359028fccaa82cefc9331c75856
kaganets.s <sv_peredriy@mail.ru>
2017-04-20 18:27:29 +0300

    39 commit module8

--- src/module8/IManageSystemImpl.java
+++ src/module8/IManageSystemImpl.java
@@ -1,53 +1,100 @@
 package module8;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by kaganets.s on 18.04.2017.
  */
-public class IManageSystemImpl implements IManageSystem {
+public class IManageSystemImpl implements IManageSystem<Food> {
 
     private Map<Food, Double> database;
 
-    @Override
-    public Object save(Object obj, double price) {
-        return null;
+    public IManageSystemImpl() {
+        this.database = new HashMap<>();
     }
 
     @Override
-    public Object save(Object obj) {
-        return null;
+    public String toString() {
+        return "IManageSystemImpl{" +
+                "database=" + database +
+                '}';
     }
 
     @Override
-    public void delete(Object obj) {
+    public Food save(Food obj, double price) {
+        database.put(obj, price);
 
+        return obj;
+    }
+
+    @Override
+    public Food save(Food obj) {
+        database.put(obj, getPrice(obj));
+        return obj;
+    }
+
+    @Override
+    public void delete(Food obj) {
+        database.remove(obj);
     }
 
     @Override
     public void deleteById(int id) {
+        Iterator<Food> iter = database.keySet().iterator();
+
+        while (iter.hasNext()) {
+            Food key = iter.next();
+            if (key.getId() == id) {
+
+                iter.remove();
+
+            }
+
+        }
 
     }
 
     @Override
-    public Object get(int id) {
+    public Food get(int id) {
+
+        Iterator<Food> iter = database.keySet().iterator();
+
+        while (iter.hasNext()) {
+            Food key = iter.next();
+            if (key.getId() == id) {
+
+                return key;
+
+            }
+        }
+
         return null;
     }
 
     @Override
-    public Double getPrice(Object obj) {
-        return null;
+    public Double getPrice(Food obj) {
+
+
+        return database.get(obj);
     }
 
     @Override
     public Set getProducts() {
-        return null;
+
+        return database.keySet();
     }
 
     @Override
     public List<Double> getPrices() {
-        return null;
+
+        List<Double> list = new LinkedList<>();
+
+        for (Double value : database.values()) {
+
+            list.add(value);
+
+        }
+
+        return list;
     }
 }
--- src/module8/Main.java
+++ src/module8/Main.java
@@ -1,21 +1,38 @@
 package module8;
 
 
-
 /**
  * Created by kaganets.s on 18.04.2017.
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(IdGenerator.generateId());
-        System.out.println(IdGenerator.generateId());
-        Food a = new Food("df",Country.POLAND, 2);
-        Food b = new Food("df",Country.ITALY, 1);
-        Food c = new Food("df",Country.FRANCE, 3);
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+
+        Food food1 = new Food("Chocolate", Country.POLAND, 365);
+        Food food2 = new Food("Spaghetti", Country.ITALY, 180);
+        Food food3 = new Food("Cheese", Country.FRANCE, 365);
+        Food food4 = new Food("Wine", Country.GERMANY, 1095);
+        Food food5 = new Food("Potato", Country.POLAND, 90);
+        Food food6 = new Food("Oil", Country.ITALY, 365);
+        Food food7 = new Food("Salo", Country.UKRAINE, 30);
+       
+
+        IManageSystem im = new IManageSystemImpl();
+
+        im.save(food1, 50);
+        im.save(food2, 100);
+        im.save(food3, 240.5);
+        im.save(food4, 123.45);
+        im.save(food5);
+        im.save(food6);
+        im.save(food7);
+        System.out.println("Get products=" + im.getProducts());
+        System.out.println("Get prices=" + im.getPrices());
+        im.delete(food7);
+        im.deleteById(4);
+        System.out.println("Find id= " + im.get(1));
+        System.out.println("Get price= " + im.getPrice(food3));
+        System.out.println("im= " + im);
     }
 }
 
