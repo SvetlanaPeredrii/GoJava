388169eb7193ae6dfc23cc35dad169794d68ceb8
kaganets.s <sv_peredriy@mail.ru>
2017-04-25 12:49:10 +0300

    40 commit module9

---
+++ src/module9/Order.java
@@ -0,0 +1,103 @@
+package module9;
+
+import Module4.Task4_1.Currency;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+/**
+ * Created by kaganets.s on 06.04.2017.
+ */
+public class Order implements Comparable<Order> {
+
+    private long id;
+    private int price;
+    private Currency currency;
+    private String itemName;
+    private String shopIdentificator;
+    private User user;
+
+    public Order(long id, int price, Currency currency, String itemName, String shopIdentificator, User user) {
+        this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.itemName = itemName;
+        this.shopIdentificator = shopIdentificator;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getShopIdentificator() {
+        return shopIdentificator;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void sortingByPrice() {
+        List<Order> orders = new ArrayList<>();
+        Collections.sort(orders, Comparator.comparingInt(Order::getPrice));
+    }
+
+    @Override
+    public int compareTo(Order o) {
+        return -((Integer) getPrice()).compareTo(o.getPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", price=" + price +
+                ", currency=" + currency +
+                ", itemName='" + itemName + '\'' +
+                ", shopIdentificator='" + shopIdentificator + '\'' +
+                ", user=" + user +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        if (id != order.id) return false;
+        if (price != order.price) return false;
+        if (currency != order.currency) return false;
+        if (!itemName.equals(order.itemName)) return false;
+        if (!shopIdentificator.equals(order.shopIdentificator)) return false;
+        return user.equals(order.user);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + price;
+        result = 31 * result + currency.hashCode();
+        result = 31 * result + itemName.hashCode();
+        result = 31 * result + shopIdentificator.hashCode();
+        result = 31 * result + user.hashCode();
+        return result;
+    }
+}
