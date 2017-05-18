eed20f8db72967f1dc377fc3e10cbf0e0fcc302f
kaganets.s <sv_peredriy@mail.ru>
2017-05-18 12:12:38 +0300

    46 commit module5 task5

--- src/module11/task4/Main.java
+++ src/module11/task4/Main.java
@@ -13,14 +13,17 @@ public class Main {
     private final static String FILENAME = "D:\\Документы\\JavaCore\\GoJava\\src\\module11\\test4.txt";
 
     public static void main(String[] args) throws IOException {
-        File file;
+        File file=null;
+        try{
         if (args.length == 0) {
             file = new File(FILENAME);
         } else {
             file = new File(args[0]);
-        }
+        }}
+        catch(Exception e){
+
 
         FileUtils fileUtils = new FileUtils(file);
         System.out.println(fileUtils.checkWord("ab"));
-    }
+    }}
 }
--- src/module5/DAOImpl.java
+++ src/module5/DAOImpl.java
@@ -33,19 +33,7 @@ class DAOImpl implements DAO {
     @Override
     public Room findById(long id) {
 
-        Room room = new Room(333, 100, 5, new Date(02 - 03 - 2017), "Hayat2", "Ukraine");
-        Room room2 = new Room(222, 100, 5, new Date(02 - 02 - 2016), "Hayat", "Ukraine");
-        Room room3 = new Room(111, 100, 5, new Date(02 - 02 - 2016), "Hayat", "Ukraine");
-        Room room4 = new Room(222, 200, 5, new Date(02 - 02 - 2016), "Hayat", "Ukraine");
-        Room room5 = new Room(555, 100, 5, new Date(02 - 02 - 2016), "Hayat", "Ukraine");
-        Room[] rooms = new Room[]{room, room2, room3, room4, room5};
-
-        for (int i = 0; i <= rooms.length - 1; i++) {
-            if (id == rooms[i].getId()) {
-
-                return rooms[i];
-            }
-        }
+        System.out.println("findById");
 
 
         return null;
