17e059878d8727ae81d8a559213c4d8bb1d8f594
Svetlana <sv_peredriy@mail.ru>
2017-06-27 10:15:16 +0300

    Add files via upload

--- Java Developer/HW1/2.sql
+++ Java Developer/HW1/2.sql
@@ -1,18 +1,11 @@
 SELECT 
-    name_project
+    name_project, MAX(TotalSalary)
 FROM
-    project_development_db.projects
-WHERE
-    id_project IN (SELECT 
-            id_project
-        FROM
-            project_development_db.developers_have_projects
-        WHERE
-            id_developer = (SELECT 
-                    id_developer
-                FROM
-                    (SELECT 
-                        id_developer, MAX(salary)
-                    FROM
-                        project_development_db.developers) AS t1))
+    (SELECT 
+        name_project, SUM(salary) TotalSalary
+    FROM
+        project_development_db.projects
+    JOIN project_development_db.developers_have_projects ON project_development_db.projects.id_project = project_development_db.developers_have_projects.id_project
+    JOIN project_development_db.developers ON project_development_db.developers.id_developer = project_development_db.developers_have_projects.id_developer
+    GROUP BY name_project) t1
 
--- Java Developer/HW1/3.sql
+++ Java Developer/HW1/3.sql
@@ -1,1 +1,10 @@
-SELECT sum(salary) FROM project_development_db.developers;
+SELECT 
+    SUM(salary)
+FROM
+    project_development_db.developers
+        JOIN
+    project_development_db.developers_have_skills ON project_development_db.developers.id_developer = project_development_db.developers_have_skills.id_developer
+        JOIN
+    project_development_db.skills ON project_development_db.skills.id_skill = project_development_db.developers_have_skills.id_skill
+WHERE
+    project_development_db.skills.programming_languages = 'Java'
